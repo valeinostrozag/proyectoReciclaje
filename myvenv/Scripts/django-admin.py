9e470cdd5ba2c646ed696195fc1b56d7ddeab161
valentina <you@example.com>
2019-11-18 13:21:54 -0300

    ambiente virtual agregado

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cetecom\desktop\nueva carpeta\recicla\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
