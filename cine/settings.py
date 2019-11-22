118b3863d2a04a182c8aa754506298adf36df1ef
Valentina Inostroza <v.inostrozag@outlook.com>
2019-11-22 10:32:39 -0300

    Cambios en el aspecto del login - Detalles del contrato

--- cine/settings.py
+++ cine/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-
+    
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
